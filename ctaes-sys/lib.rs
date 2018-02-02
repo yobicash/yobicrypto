0bef2a57eda6b6852801e4cc07937a194982e332
chritchens <chritchens@gmail.com>
2018-02-02 04:17:18 +0100

    Add missing license info

--- ctaes-sys/lib.rs
+++ ctaes-sys/lib.rs
@@ -1,3 +1,13 @@
+// Copyright 2018 Yobicash Ltd. See the COPYRIGHT file at the top-level directory
+// of this distribution.
+//
+// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>
+// and the Apache 2.0 license <LICENSE-APACHE or https://opensource.org/licenses/Apache-2.0>.
+// This file may not be copied, modified, or distributed except according to those
+// terms.
+
+//! Ctaes native bindings.
+
 #[macro_use]
 extern crate failure;
 extern crate typenum;
--- src/zkp.rs
+++ src/zkp.rs
@@ -1,7 +1,8 @@
 // Copyright 2018 Yobicash Ltd. See the COPYRIGHT file at the top-level directory
 // of this distribution.
 //
-// Licensed under the MIT license <LICENSE or http://opensource.org/licenses/MIT>.
+// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>
+// and the Apache 2.0 license <LICENSE-APACHE or https://opensource.org/licenses/Apache-2.0>.
 // This file may not be copied, modified, or distributed except according to those
 // terms.
 
